ebc0fd83cae6e09630ef0b8b35985a63507c2288
naeimrezaeian <naeim.rezaeian@hotmail.com>
2025-02-08 12:27:53 +0300

    change install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,20 @@ set -e
 echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
 
+
+
+echo "Allowing traffic on port 80..."
+sudo ufw allow 80/tcp
+
+if [ $? -eq 0 ]; then
+  echo "Port 80 opened successfully."
+else
+  echo "Error: Failed to open port 80."
+fi
+echo "Restarting UFW..."
+sudo ufw disable
+sudo ufw enable
+
 echo "Installing NGINX..."
 sudo apt install nginx -y
 
@@ -13,11 +27,18 @@ echo "Starting and enabling NGINX..."
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
+
 echo "Cloning website from GitHub..."
 GIT_REPO_URL="https://github.com/naeimrezaeian/rudn3.git"
 TARGET_DIR="/var/www/assistant"
-sudo rm -rf $TARGET_DIR
+TEMP_DIR="/tmp/rudn3_clone"
+
 sudo git clone $GIT_REPO_URL $TARGET_DIR
+
+
+echo "Copying files from website folder to $TARGET_DIR"
+sudo mkdir -p "$TARGET_DIR"
+sudo cp -r "$TEMP_DIR/web/"* "$TARGET_DIR"
 
 echo "Setting permissions for web directory..."
 sudo chown -R www-data:www-data $TARGET_DIR
@@ -48,6 +69,9 @@ sudo nginx -t
 
 echo "Restarting NGINX..."
 sudo systemctl restart nginx
+
+echo "Cleaning up temporary directory..."
+rm -rf "$TEMP_DIR"
 
 echo "Installation and setup complete!"
 echo "Don't forget to update your /etc/hosts file with:"
